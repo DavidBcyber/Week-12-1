87b311ad0c6669352aa641752a7c40ba67bad64b
David Bustos <dbustos28@amarillocollege.com>
2025-04-13 17:57:03 -0500

    first commit

---
+++ python.py
@@ -0,0 +1,11 @@
+class Moblie:
+    def __init__(self,Brand,Model,Storage,Price): #the mobile class details
+        self.Brand = Brand
+        self.Model = Model
+        self.Storage = Storage
+        self.Price = Price
+    
+
+p1 = Moblie("apple", "iphone10", 250, 1000) #phone 1
+p2 = Moblie("Samsung", "Fold 3",  280, 850) #phone 2
+    
